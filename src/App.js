0991f3e72d017dc3c98766e8091f9bd33eb81f4b
My Le <irisle412@gmail.com>
2022-03-23 23:13:06 +0700

    push weather project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const api = {
+  key: "986f593b23d55a7759b1c5dbdce49fc4",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (!searchCity) return;
+
+      setLoading(true);
+      // process
+      try {
+        const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.ok) {
+          setWeatherInfo(
+            `${data.name}, ${data.sys.country}\n${data.weather[0].description}\n${data.main.temp}°C`
+          );
+
+          setErrorMessage("");
+        } else {
+          setErrorMessage(data.message); //cú pháp theo err api trả về
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+
+      setLoading(false);
+    };
+    fetchWeatherData();
+  }, [searchCity]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Weather App</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              placeholder="Enter City"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+          </div>
+          <button className="btn">Search</button>
+        </form>
+        {loading ? (
+          <div className="load">loading.....</div>
+        ) : (
+          <>
+            {errorMessage ? (
+              <div className="load err">{errorMessage}</div>
+            ) : (
+              <div className="load weather">{weatherInfo}</div>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
